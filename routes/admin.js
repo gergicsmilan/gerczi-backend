47658aaab7350899915ca2a802f74305f4055d97
gergicsmilan <gergics.milan95@gmail.com>
2020-04-28 22:32:15 +0200

    created add new product function for admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,1 +1,13 @@
-// Admin user's routes
+const express = require("express");
+
+const router = express.Router();
+
+const admintController = require("../controllers/admin");
+
+//method: GET
+
+//method: POST
+//path: "/admin/add-product"
+router.post("/add-product", admintController.addProduct);
+
+module.exports = router;
