c5102f74be39c8dbaceb2fdc42ba60874f68413f
gergicsmilan <gergics.milan95@gmail.com>
2020-04-28 13:43:35 +0200

    created test user, established db connection

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const restaurantRoutes = require("./routes/restaurant");
+const User = require("./models/user");
 
 const app = express();
 
@@ -16,6 +18,39 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  User.findById("5ea813dd40cca22b446fc17b")
+    .then((user) => {
+      res.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use(restaurantRoutes);
 
-app.listen(6969);
+mongoose
+  .connect(
+    "mongodb+srv://gerczi:gerczi69@restaurantdb-iax4d.mongodb.net/restaurant?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then((result) => {
+    // allows to have only one user for testing, and also creates one...
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          email: "test@gmail.com",
+          firstName: "Milák",
+          lastName: "Gerczi",
+          phoneNumber: "30/6969699",
+          role: "user",
+          address: { zip: 6969, city: "Budapest", street: "Big street 69." },
+        });
+        user.save();
+      }
+    });
+    console.log("Successfully connected to DB!");
+
+    app.listen(6969);
+  })
+  .catch((err) => console.log(err));
