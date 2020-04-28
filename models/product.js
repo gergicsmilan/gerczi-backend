27310dc925b1277117d97e594ecb6ee62b2c23ad
gergicsmilan <gergics.milan95@gmail.com>
2020-04-28 22:31:40 +0200

    added product model

---
+++ models/product.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const productSchema = Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  ingredients: [{ type: String, required: true }], // for filtering the product should implement Category, Ingredient modell
+});
+
+module.exports = mongoose.model("Product", productSchema);
