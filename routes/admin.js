34cba31130c9534cb76be04e75be72f7f4703fce
gergicsmilan <gergics.milan95@gmail.com>
2020-04-29 19:56:58 +0200

    fixed a typo in admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
-const router = express.Router();
+const adminController = require("../controllers/admin");
 
-const admintController = require("../controllers/admin");
+const router = express.Router();
 
 //method: GET
 
 //method: POST
 //path: "/admin/add-product"
-router.post("/add-product", admintController.addProduct);
+router.post("/add-product", adminController.addProduct);
 
 module.exports = router;
