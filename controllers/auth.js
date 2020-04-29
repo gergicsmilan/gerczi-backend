1c4be7253a6b0b034cf2dc2a10327f6daa9b4b32
Akos Czifra <akosczifraa@gmail.com>
2020-04-29 21:25:39 +0200

    added signIn and refactored signUp

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,46 +8,109 @@ exports.signUp = (req, res, next) => {
   const password = req.body.password;
   const lastName = req.body.lastName;
   const firstName = req.body.firstName;
+  const emailInUseError = new Error("Email already in use!");
+  emailInUseError.statusCode = 401;
 
   User.findOne({ email: email })
     .then((user) => {
       if (user) {
-        return res.status(422).json({ message: "Email already in use!" });
+        throw emailInUseError;
+        //return res.status(401).json({ message: "Email already in use!" });
         /* const error = new Error("Email already in use!");
-        error.statusCode = 422;
+        error.statusCode = 401;
         throw error; */
         /* return Promise.reject("Promise rejected!"); */
-      } else {
-        bcrypt.hash(password, 12).then((hashedPassword) => {
-          const newUser = new User({
-            email: email,
-            password: hashedPassword,
-            lastName: lastName,
-            firstName: firstName,
-          });
-          return newUser.save().then((user) => {
-            // generate jwt
-            /* const token = jwt.sign(
-              {
-                email: user.email,
-                lastName: user.lastName,
-                firstName: user.firstName,
-              },
-              "mysecretkey"
-            ); */
-            res.status(201).json({ message: "User successfully created!" });
-          });
-        });
       }
+      bcrypt.hash(password, 12).then((hashedPassword) => {
+        const newUser = new User({
+          email: email,
+          password: hashedPassword,
+          lastName: lastName,
+          firstName: firstName,
+        });
+        return newUser.save();
+      })
+      /* .then((user) => {
+        console.log(`${user}`);
+        res.status(201).json({ message: "User successfully created!" });
+      }); */
+    })
+    .then(result => {
+      console.log(`${email} user has been created!`);
+      res.status(201).json({ message: "User successfully created!" });
     })
     /* .then((user) => {
       console.log(`new user: ${user}`);
-      if (res.statusCode !== 422) {
+      if (res.statusCode !== 401) {
         res.status(201).json({ message: "User successfully created!" });
       }
     }) */
-    .catch((err) => {
-      /* if (err.statusCode === 422) res.status(422).json({ message: err }); */
-      res.status(500).json({ error: err.message });
-    });
+    .catch(err => {
+      if (err.statusCode === 401) {
+        console.log(err.message);
+        res.status(err.statusCode).json({ error: err.message });
+      } else {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+      }
+      /* if (err.statusCode === 401) res.status(401).json({ message: err }); */
+    })
 };
+
+exports.signIn = (req, res, next) => {
+  let loadedUser;
+  const email = req.body.email;
+  const password = req.body.password;
+  const authError = new Error("Invalid email or password!");
+  authError.statusCode = 401;
+
+  User.findOne({ email: email }).then(user => {
+    if (!user) {
+      throw authError;
+      //return res.status(401).json({ message: "Invalid email or password!" });
+    }
+    loadedUser = user;
+    return bcrypt.compare(password, loadedUser.password);
+  }).then(validPassword => {
+    if (!validPassword) {
+      throw authError;
+    }
+    const token = jwt.sign(
+      {
+        email: loadedUser.email,
+        lastName: loadedUser.lastName,
+        firstName: loadedUser.firstName,
+      },
+      "mysecretkey"
+    );
+    console.log("JWT succesfully created!");
+    res.status(200).json({ jwt: token });
+    /* // generate jwt
+    if (loadedUser && validPassword && res.status !== 401) {
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          lastName: loadedUser.lastName,
+          firstName: loadedUser.firstName,
+        },
+        "mysecretkey"
+      );
+      console.log("JWT succesfully created!");
+      return res.status(200).json({jwt: token});
+    } else {
+      if (!validPassword && res.status !== 401) {
+        console.log("Invalid email or password!")
+        res.status(401).json({message: "Invalid email or password!"})
+      }
+    } */
+  })
+    .catch(err => {
+      if (err.statusCode === 401) {
+        console.log(err.message);
+        res.status(err.statusCode).json({ error: err.message });
+      } else {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+      }
+    })
+}
