d25cdf21f1bb8c3c75d1df19d858cbbd6d9f5319
gergicsmilan <gergics.milan95@gmail.com>
2020-05-01 00:13:44 +0200

    added input validation middleware to sign-up, sign-in routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { body } = require("express-validator");
 
 const authController = require("../controllers/auth");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -9,10 +11,51 @@ const router = express.Router();
 
 // Method: POST
 // path: "/auth/sign-up"
-router.post("/sign-up", authController.signUp);
+router.post(
+  "/sign-up",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email!")
+      .normalizeEmail()
+      .custom((value) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (user) {
+            return Promise.reject("This email is already in use!");
+          }
+        });
+      }),
+    body("password", "Password should contain minimum 5 characters!")
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body(
+      "confirmPassword",
+      "Confirm password should match with other password!"
+    )
+      .exists()
+      .custom((value, { req }) => value === req.body.password),
+    body(
+      "firstName",
+      "First name should contain only alphabetical characters!"
+    ).isAlpha(),
+    body(
+      "lastName",
+      "Last name should contain only alphabetical characters!"
+    ).isAlpha(),
+  ],
+  authController.signUp
+);
 
 // path: "/auth/sign-in"
-router.post("/sign-in", authController.signIn);
-
+router.post(
+  "/sign-in",
+  [
+    body("email").isEmail().withMessage("Invalid email or password!"),
+    body("password", "Password should contain minimum 5 characters!")
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.signIn
+);
 
 module.exports = router;
