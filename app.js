a086f7fb7df77b4a7a046c77903f02ce95491ed8
Akos Czifra <akosczifraa@gmail.com>
2020-05-04 10:13:25 +0200

    added default statusCode to error handling middleware

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use("/auth", authRoutes);
 app.use((error, req, res, next) => {
   console.log(error);
 
-  const status = error.statusCode;
+  const status = error.statusCode || 500;
   const message = error.message;
   res.status(status).json({ errorMessage: message });
 });
