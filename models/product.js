61e79627c58f7f24b4f28bb0fba12499697de435
Akos Czifra <akosczifraa@gmail.com>
2020-05-05 17:06:16 +0200

    added imageUrl to productSchema

--- models/product.js
+++ models/product.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const productSchema = Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
+  imageUrl: { type: String, required: true },
   ingredients: [{ type: String, required: true }], // for filtering the product should implement Category, Ingredient modell
 });
 
