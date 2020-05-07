77d6ab590c53ec62e82e6f06bdc9574494074bc1
gergicsmilan <gergics.milan95@gmail.com>
2020-05-07 19:32:32 +0200

    refactored addProduct function, product automatically added to categories if necessary, working with async/await instead of .then

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,43 +1,87 @@
 const { validationResult } = require('express-validator');
+const mongoose = require('mongoose');
 
-const Product = require("../models/product");
+const Product = require('../models/product');
+const Category = require('../models/category');
 
-exports.addProduct = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, incorrect data!')
-    error.statusCode = 422;
-    throw error;
-  };
-  if (!req.file) {
-    const error = new Error('No image provided!')
-    error.statusCode = 422;
-    throw error;
+exports.addProduct = async (req, res, next) => {
+  const categoryIds = req.body.categoryIds ? [...req.body.categoryIds] : null;
+
+  try {
+    const addProductResult = await Product.create({
+      name: req.body.productName,
+      price: req.body.productPrice,
+      ingredients: [...req.body.productIngredients],
+      imageUrl: req.file ? req.file.path.replace('\\', '/') : null,
+      categoryIds: categoryIds,
+    });
+    if (categoryIds) {
+      const updateQueries = categoryIds.map((categoryId) => ({
+        updateOne: {
+          filter: { _id: categoryId },
+          update: {
+            $addToSet: { products: addProductResult._id },
+          },
+        },
+      }));
+      if (updateQueries.length > 0) await Category.bulkWrite(updateQueries);
+    }
+  } catch (err) {
+    next(err);
   }
-  const productName = req.body.productName;
-  const productPrice = req.body.productPrice;
-  const imageUrl = req.file.path.replace("\\", "/");
-  let productIngredients = [];
-  if (req.body.productIngredients) {
-    productIngredients = [...req.body.productIngredients];
+  console.log('Product successfully created!');
+  return res.status(200).json({ success: true });
+};
+
+exports.addCategory = (req, res, next) => {
+  const categoryName = req.body.categoryName;
+  const categoryPrice = req.body.categoryPrice;
+  let imageUrl;
+  if (req.file) {
+    imageUrl = req.file.path.replace('\\', '/');
   }
-  const newProduct = new Product({
-    name: productName,
-    price: productPrice,
-    imageUrl: imageUrl,
-    ingredients: productIngredients,
+
+  const newCategory = new Category({
+    name: categoryName,
+    price: categoryPrice,
   });
-  newProduct
+
+  if (imageUrl) {
+    newCategory.imageUrl = imageUrl;
+  }
+
+  newCategory
     .save()
     .then((result) => {
-      console.log('Product successfully created!')
-      res.status(201).json({ message: "Product successfully created!", product: result })
-
+      console.log('Category successfully created!');
+      res
+        .status(201)
+        .json({ message: 'Category successfully created!', category: result });
     })
     .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
-      next(err)
+      next(err);
     });
+};
+
+exports.addProductToCategory = async (req, res, next) => {
+  const productId = req.body.productId;
+  const categoryId = req.body.categoryId;
+  let result;
+  try {
+    result = await Category.updateOne(
+      { _id: mongoose.Types.ObjectId(categoryId) },
+      {
+        $addToSet: { products: mongoose.Types.ObjectId(productId) },
+      }
+    );
+    if (result.nModified === 0) {
+      throw new Error('Update failed!');
+    }
+  } catch (err) {
+    return next(err);
+  }
+  return res.status(202).json({ success: true });
 };
