8c0c58098942a6553598b7e49391e8751660100f
Akos Czifra <akosczifraa@gmail.com>
2020-05-08 19:38:39 +0200

    added security middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const uuidv4 = require('uuid').v4;
+const lusca = require('lusca');
 
 const restaurantRoutes = require('./routes/restaurant');
 const adminRoutes = require('./routes/admin');
@@ -39,6 +40,12 @@ app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(lusca({
+  xframe: 'SAMEORIGIN',
+  xssProtection: true,
+}));
+app.disable('x-powered-by');
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // allows access to any domain (frontend: localhost:3000)
