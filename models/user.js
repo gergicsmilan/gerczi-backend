7ba79c8f356190134e60d4af3cb8a5df4fa30735
Akos Czifra <akosczifraa@gmail.com>
2020-05-08 19:40:14 +0200

    added validation and error handling

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
+const Product = require('./product')
 
 const Schema = mongoose.Schema;
 
@@ -8,7 +10,7 @@ const userSchema = Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   phoneNumber: { type: String, default: null },
-  role: { type: String, default: "user" },
+  role: { type: String, default: 'user' },
   address: {
     zip: { type: Number, default: null },
     city: { type: String, default: null },
@@ -19,7 +21,7 @@ const userSchema = Schema({
       {
         productId: {
           type: Schema.Types.ObjectId,
-          ref: "Product",
+          ref: 'Product',
           required: true,
         },
         quantity: { type: Number, required: true },
@@ -28,13 +30,24 @@ const userSchema = Schema({
   },
 });
 
-userSchema.methods.addProductToCart = function (productId) {
+userSchema.methods.addProductToCart = async function (productId) {
+  try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      const error = new Error('Invalid productId!');
+      error.statusCode = 422;
+      throw error;
+    }
+  } catch (err) {
+    throw err;
+  }
+
   const cartProductIndex = this.cart.products.findIndex((cartProduct) => {
     return cartProduct.productId.toString() === productId.toString();
   });
+
   let productQuantity = 1;
   const updatedCartProducts = [...this.cart.products];
-
   if (cartProductIndex >= 0) {
     productQuantity = this.cart.products[cartProductIndex].quantity + 1;
     updatedCartProducts[cartProductIndex].quantity = productQuantity;
@@ -44,12 +57,16 @@ userSchema.methods.addProductToCart = function (productId) {
       quantity: productQuantity,
     });
   }
-
   this.cart.products = updatedCartProducts;
   return this.save();
 };
 
 userSchema.methods.removeProductFromCart = function (productId) {
+  if (this.cart.products.length === 0) {
+    const error = new Error('Cart is empty!');
+    error.statusCode = 422;
+    throw error;
+  }
   const updatedCartProducts = this.cart.products.filter((item) => {
     return item.productId.toString() !== productId.toString();
   });
@@ -57,4 +74,4 @@ userSchema.methods.removeProductFromCart = function (productId) {
   return this.save();
 };
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
