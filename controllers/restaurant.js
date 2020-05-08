6c3c40d99b00eff5a6c278a86e281f62268c7ea6
Akos Czifra <akosczifraa@gmail.com>
2020-05-08 15:16:27 +0200

    refactored restaurant controller

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -1,36 +1,33 @@
 const Product = require("../models/product");
-const User = require("../models/user");
 
 exports.getHome = (req, res, next) => {
-  res.status(200).json({ message: "Test get request" });
+  return res.status(200).json({ success: true });
 };
 
-exports.getProducts = (req, res, next) => {
-  Product.find()
-    .then((products) => res.status(200).json({ products }))
-    .catch((err) => console.log(err));
+exports.getProducts = async (req, res, next) => {
+  let products;
+  try {
+    products = await Product.find();
+  } catch (err) {
+    return next(err);
+  }
+  return res.status(200).json({ success: true, products: products })
 };
 
-exports.postAddCartProduct = (req, res, next) => {
-  const productId = req.body.productId;
-  req.user
-    .addProductToCart(productId)
-    .then((result) => {
-      res
-        .status(200)
-        .json({ message: `Product: ${productId} is added to cart!` });
-    })
-    .catch((err) => console.log(err));
+exports.postAddCartProduct = async (req, res, next) => {
+  try {
+    await req.user.addProductToCart(req.body.productId);
+  } catch (err) {
+    return next(err)
+  }
+  return res.status(200).json({ success: true })
 };
 
-exports.postDeleteCartProduct = (req, res, next) => {
-  const productId = req.body.productId;
-  req.user
-    .removeProductFromCart(productId)
-    .then((result) =>
-      res
-        .status(200)
-        .json({ message: `Product: ${productId} is removed from cart!` })
-    )
-    .catch((err) => console.log(err));
+exports.postDeleteCartProduct = async (req, res, next) => {
+  try {
+    await req.user.removeProductFromCart(req.body.productId);
+  } catch (err) {
+    return next(err)
+  }
+  return res.status(200).json({ success: true })
 };
