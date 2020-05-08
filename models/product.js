a54596288674b84dcbd70cf6b66c3124f3dc5abe
Akos Czifra <akosczifraa@gmail.com>
2020-05-08 14:17:28 +0200

    changed categoryIds default value to []

--- models/product.js
+++ models/product.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const productSchema = Schema({
   categoryIds: [
-    { type: Schema.Types.ObjectId, ref: 'Category', default: null },
+    { type: Schema.Types.ObjectId, ref: 'Category', default: [] },
   ],
   name: { type: String, required: true },
   price: { type: Number, required: true },
