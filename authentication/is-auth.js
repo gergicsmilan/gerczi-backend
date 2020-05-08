1a6eafcf71ae5487badd3740ed0d059e24e96869
gergicsmilan <gergics.milan95@gmail.com>
2020-05-08 14:09:02 +0200

    created authentication middleware

---
+++ authentication/is-auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(req.get('Authorization'), 'mysecretkey');
+  } catch (err) {
+    throw err;
+  }
+  if (!decodedToken) {
+    const error = new Error('Not authenticated!');
+    error.statusCode = 401;
+    throw error;
+  }
+  // user roles needed (admin, user)..
+  req.userId = decodedToken.userId;
+  next();
+};
