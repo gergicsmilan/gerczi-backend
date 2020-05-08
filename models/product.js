af1b956ec9c76e8ae74a2cee7dab66561310e9a8
Akos Czifra <akosczifraa@gmail.com>
2020-05-08 20:43:48 +0200

    refactored field placement

--- models/product.js
+++ models/product.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = Schema({
-  categoryIds: [
-    { type: Schema.Types.ObjectId, ref: 'Category', default: [] },
-  ],
   name: { type: String, required: true },
   price: { type: Number, required: true },
   imageUrl: { type: String, default: null },
   ingredients: [{ type: String, required: true }],
+  categoryIds: [
+    { type: Schema.Types.ObjectId, ref: 'Category', default: [] },
+  ],
 });
 
 module.exports = mongoose.model('Product', productSchema);
