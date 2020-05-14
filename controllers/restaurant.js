f2e4263887f823abf8e27bf155887c10554e9e9f
gergicsmilan <gergics.milan95@gmail.com>
2020-05-14 19:06:42 +0200

    created getProductsByCategory middleware

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 
-const Product = require("../models/product");
+const Product = require('../models/product');
+const Category = require('../models/category');
 
 exports.getHome = (req, res, next) => {
   return res.status(200).json({ success: true });
@@ -13,7 +14,44 @@ exports.getProducts = async (req, res, next) => {
   } catch (err) {
     return next(err);
   }
-  return res.status(200).json({ success: true, products: products })
+  return res.status(200).json({ success: true, data: products });
+};
+
+exports.getCategories = async (req, res, next) => {
+  let categories;
+  try {
+    categories = await Category.find().populate('products', '-categoryIds');
+  } catch (err) {
+    return next(err);
+  }
+
+  return res.status(200).json({ success: true, data: categories });
+};
+
+exports.getProductsByCategory = async (req, res, next) => {
+  const errors = validationResult(req);
+  let products;
+  try {
+    if (!errors.isEmpty()) {
+      const error = new Error(errors.array()[0].msg);
+      error.statusCode = 422;
+      throw error;
+    }
+
+    products = await Category.findById(req.params.id)
+      .select('products')
+      .populate('products', '-categoryIds');
+
+    if (!products) {
+      const error = new Error(`Category: ${req.categoryId} not found!`);
+      error.statusCode = 422;
+      throw error;
+    }
+  } catch (err) {
+    return next(err);
+  }
+
+  return res.status(200).json({ success: true, data: products });
 };
 
 exports.postAddCartProduct = async (req, res, next) => {
@@ -26,9 +64,9 @@ exports.postAddCartProduct = async (req, res, next) => {
     }
     await req.user.addProductToCart(req.body.productId);
   } catch (err) {
-    return next(err)
+    return next(err);
   }
-  return res.status(200).json({ success: true })
+  return res.status(200).json({ success: true });
 };
 
 exports.postDeleteCartProduct = async (req, res, next) => {
@@ -41,7 +79,7 @@ exports.postDeleteCartProduct = async (req, res, next) => {
     }
     await req.user.removeProductFromCart(req.body.productId);
   } catch (err) {
-    return next(err)
+    return next(err);
   }
-  return res.status(200).json({ success: true })
+  return res.status(200).json({ success: true });
 };
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,22 +1,43 @@
-const express = require("express");
-const { body } = require("express-validator");
+const express = require('express');
+const { param, body } = require('express-validator');
 
-const restaurantController = require("../controllers/restaurant");
+const restaurantController = require('../controllers/restaurant');
 
 const router = express.Router();
 
 // method:GET
 // path:"/"
-router.get("/", restaurantController.getHome);
+router.get('/', restaurantController.getHome);
+
+// path:"/categories"
+router.get('/categories', restaurantController.getCategories);
 
 //path:"/products"
-router.get("/products", restaurantController.getProducts);
+router.get('/products', restaurantController.getProducts);
+
+//path:"/products-by-category"
+router.get(
+  '/products-by-category/:id',
+  param('id', 'Invalid or missing category id!')
+    .exists()
+    .isString()
+    .isLength({ min: 24, max: 24 }),
+  restaurantController.getProductsByCategory
+);
 
 // method:POST
 // path:"/cart/add-product"
-router.post("/cart/add-product", body('productId').exists().isString().isLength({ min: 24, max: 24 }), restaurantController.postAddCartProduct);
+router.post(
+  '/cart/add-product',
+  body('productId').exists().isString().isLength({ min: 24, max: 24 }),
+  restaurantController.postAddCartProduct
+);
 
 // path:"/cart/remove-product"
-router.post("/cart/remove-product", body('productId').exists().isString().isLength({ min: 24, max: 24 }), restaurantController.postDeleteCartProduct);
+router.post(
+  '/cart/remove-product',
+  body('productId').exists().isString().isLength({ min: 24, max: 24 }),
+  restaurantController.postDeleteCartProduct
+);
 
 module.exports = router;
