a1aef1c5e76ce01fa89b1f3a376e1c0a2d96b7c4
gergicsmilan <gergics.milan95@gmail.com>
2020-06-19 10:56:53 +0200

    created order model and schema

---
+++ models/order.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Products = require('./product');
+
+const Schema = mongoose.Schema;
+
+const orderSchema = (Schema = {
+  user: { userId: { type: Schema.Types.ObjectId, ref: 'User', required: true } },
+  contactInfo: {
+    email: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phoneNumber: { type: String, default: null },
+  },
+  shippinginfo: {
+    zip: { type: Number, required: true },
+    city: { type: String, required: true },
+    street: { type: String, required: true },
+  },
+  orderedItems: {
+    products: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
+
+module.exports = mogoose.model('Order', orderSchema);
